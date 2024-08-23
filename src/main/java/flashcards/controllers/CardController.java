5bc6e14dc95882fff96df5f0ff3f59c79ada015e
AnniaKuzmina <107867033+anniakuz@users.noreply.github.com>
2024-08-23 14:01:05 +0200

    card, response to controller

--- src/main/java/flashcards/controllers/CardController.java
+++ src/main/java/flashcards/controllers/CardController.java
@@ -1,6 +1,7 @@
 package flashcards.controllers;
 
 import flashcards.entities.Card;
+import flashcards.mapper.CardMapper;
 import flashcards.requests.card.CreateCardRequest;
 import flashcards.requests.card.HashtagsRequest;
 import flashcards.requests.card.UpdateCardRequest;
@@ -26,8 +27,9 @@ public class CardController {
     @PostMapping("")
     public ResponseEntity<?> addCard( @Valid @RequestBody CreateCardRequest request){
 
-        CardResponse response = cardService.addCard(request.getFront(),
+        Card card = cardService.addCard(request.getFront(),
                 request.getBackside(), request.getCollection_id(), request.getHashtags());
+        CardResponse response = CardMapper.toResponse(card);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
@@ -40,59 +42,71 @@ public class CardController {
     @PutMapping("/{id}")
     public ResponseEntity<?> editCard(@PathVariable Integer id, @RequestBody UpdateCardRequest request){
 
-        CardResponse response = cardService.updateCard(request.getFront(),
+        Card card= cardService.updateCard(request.getFront(),
                 request.getBackside(), id);
+        CardResponse response = CardMapper.toResponse(card);
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<?> getCard(@PathVariable Integer id){
-        CardResponse response = cardService.getCardById(id);
+        Card card = cardService.getCardById(id);
+        CardResponse response = CardMapper.toResponse(card);
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
     @GetMapping("")
     public ResponseEntity<?> getAllCards(){
-        List<CardResponse> response = cardService.getAllCards();
+        List<Card> cards= cardService.getAllCards();
+
+        List<CardResponse> response = CardMapper.toResponseList(cards);
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
     @GetMapping("/collection/{collection_id}")
     public ResponseEntity<?> getAllCardsByCollection(@PathVariable Integer collection_id){
-        List<CardResponse> response = cardService.getAllCardsByCollection(collection_id);
+        List<Card> cards = cardService.getAllCardsByCollection(collection_id);
+        List<CardResponse> response = CardMapper.toResponseList(cards);
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
     @PatchMapping("/favourite/{id}")
     public ResponseEntity<?> changeFavouriteCard(@PathVariable Integer id){
-        CardResponse response = cardService.changeFavourite(id);
+        Card card = cardService.changeFavourite(id);
+        CardResponse response = CardMapper.toResponse(card);
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
     @GetMapping("/random")
     public ResponseEntity<?> getRandomCards(){
-        List<CardResponse> response = cardService.getRandomCards();
+        List<Card> cards = cardService.getRandomCards();
+
+        List<CardResponse> response = CardMapper.toResponseList(cards);
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
     //get all favourite
     @GetMapping("/favourite")
     public ResponseEntity<?> getFavouriteCards(){
-        List<CardResponse> response = cardService.getAllFavourite();
+        List<Card> cards = cardService.getAllFavourite();
+
+        List<CardResponse> response = CardMapper.toResponseList(cards);
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
     @GetMapping("/hashtag")
     public ResponseEntity<?> getCardsByHashtag(
             @RequestParam(name = "hashtag", required = true) String hashtag){
-        List<CardResponse> response = cardService.getCardsByHashtag(hashtag);
+        List<Card> cards = cardService.getCardsByHashtag(hashtag);
+        List<CardResponse> response = CardMapper.toResponseList(cards);
         return ResponseEntity.status(HttpStatus.OK).body(response);
 
     }
 
     @PostMapping("/{card_id}/hashtag")
     public ResponseEntity<?> addHashtag(@RequestBody HashtagsRequest request, @PathVariable Integer card_id){
-        CardResponse response = cardService.addHashtag(request.getHashtags(), card_id);
+        Card card = cardService.addHashtag(request.getHashtags(), card_id);
+        CardResponse response = CardMapper.toResponse(card);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
